6e98616fb6976da7f0e35e62f05afaf3632f0df2
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-11-08 15:13:15 -0500

    se agrego funcionalidad al reloj del nivel 2

--- javascript/nivel2_script.js
+++ javascript/nivel2_script.js
@@ -1329,19 +1329,27 @@ if (document.getElementById("topo12").onclick){
 /**************************************************************************** */
 // RELOJ
 ////
-momentoActual = new Date()
-hora = momentoActual.getHours()
-minuto = momentoActual.getMinutes()
-segundo = momentoActual.getSeconds()
-var horaImprimible = hora + " : " + minuto + " : " + segundo
 function mueveReloj(){
     momentoActual = new Date()
     hora = momentoActual.getHours()
     minuto = momentoActual.getMinutes()
     segundo = momentoActual.getSeconds()
 
+//.. agregando estos parametros 
+    str_segundo = new String (segundo)
+    if (str_segundo.length == 1)
+       segundo = "0" + segundo
+
+    str_minuto = new String (minuto)
+    if (str_minuto.length == 1)
+       minuto = "0" + minuto
+
+    str_hora = new String (hora)
+    if (str_hora.length == 1)
+       hora = "0" + hora
+
     horaImprimible = hora + " : " + minuto + " : " + segundo
 
     document.form_reloj.reloj.value = horaImprimible
+    setTimeout("mueveReloj()",1000)
 }
-setTimeout("mueveReloj()",1000)
